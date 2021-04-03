e91ee33ad3b17a9aa125b1302f52fa060e03a459
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-04-03 15:08:36 +0300

    Updates

--- static/js/main.134f6712.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var c=n(1),o=n.n(c),r=n(3),s=n.n(r),i=(n(8),n(0));var h=function(){return Object(i.jsx)("h1",{children:"hello"})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(h,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.134f6712.chunk.js.map
